7daa889ebbfa8a5f6d6a4700859c983b21f641ed
Sare@20 <umapnishad1@gmail.com>
2024-07-08 16:00:28 +0530

    Database Connected Successfully

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
-import express  from 'express';
-const app = express()
-const port = 3000
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect(
+  process.env.MONGOOSEURL
+).then(()=>{
+    console.log('Database Connected Successfully');
+}).catch((err)=>{
+    console.log(err);
+})
 
-app.listen(port, () => {
-    console.log(`Server Running Successfully on port ${port} `)
-  })
+
+const app = express();
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server Running Successfully on ${PORT}`);
+});
