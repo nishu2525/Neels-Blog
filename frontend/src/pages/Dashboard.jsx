fc0f959be12ce729671546d80a3ba5ca15e57f8d
Sare@20 <umapnishad1@gmail.com>
2024-07-11 17:46:34 +0530

    sidebar dasboard UI

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom"
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
+
 export default function Dashboard() {
+  const location =useLocation();
+  const [tab,setTab] =useState('')
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
   return (
-    <div>Dashboard</div>
+    <div className="h-full flex flex-col md:flex-row">
+      {/* Sidebar */}
+      <div className="md:w-56">
+        <DashSidebar />
+      </div>
+
+      {/* Rigt Side */}
+      <div>
+        {tab== 'profile' &&  <DashProfile/>}
+      </div>
+    </div>
   )
 }
 
