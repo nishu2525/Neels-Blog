a90ab255d10c97d848e9d6745d54ae02d92af800
Sare@20 <umapnishad1@gmail.com>
2024-07-14 22:59:05 +0530

    Delete post from Dashboard

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { create, getpost } from '../controllers/post.controller.js';
+import { create, deletepost, getpost } from '../controllers/post.controller.js';
 const router =express.Router();
 
 router.post('/create/:userId', verifyToken,create);
 router.get('/getposts',getpost)
+router.delete('/deletepost/:postId/:userId', verifyToken, deletepost)
 
 export default router;
