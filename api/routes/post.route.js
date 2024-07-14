f7fa029b1c38b67d4f0b1a3a0c284457762e7429
Sare@20 <umapnishad1@gmail.com>
2024-07-14 20:52:28 +0530

    created api route for posts count

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { create } from '../controllers/post.controller.js';
+import { create, getpost } from '../controllers/post.controller.js';
 const router =express.Router();
 
 router.post('/create/:userId', verifyToken,create);
+router.get('/getposts',getpost)
 
 export default router;
